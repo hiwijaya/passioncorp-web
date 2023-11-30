c5f118b408e472d76c7320d61b8baff55d55a178
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2023-11-30 13:44:17 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Passion Corp`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
