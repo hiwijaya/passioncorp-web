6a0fa1ec6d2bd9ff821080b12740e88cc3a55656
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2023-12-06 03:23:28 +0700

    feat: change logo

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,17 +6,18 @@ const Navigation = () => {
     <div className="absolute top-0 left-0 z-10 inline-flex justify-center w-full h-20">
         <div className="inline-flex items-center justify-between w-full max-w-7xl">
           <a href="/">
-            <div className="flex flex-row items-center text-white">
-              <StaticImage className="w-16 mr-1" src="../images/logo-white.png" alt="logo"/>
-              Passion Corp Indonesia
+            <div className="flex flex-row items-center text-white font-medium">
+              <StaticImage className="w-12 mr-1" src="../images/logo-passion.png" alt="logo"/>
+              Passion Corp. Indonesia
             </div>
           </a>
           <div>
             <nav className="inline-flex text-white gap-10 mr-10">
               <a href="/">Services</a>
-              <a href="/">Pricing</a>
+              <a href="/">Portfolio</a>
               <a href="/">About</a>
               <a href="/">Contact</a>
+              <a href="/">FAQ</a>
             </nav>
             <button className="rounded-xl bg-sky-500 text-white px-6 py-3">Hire Us</button>
           </div>
