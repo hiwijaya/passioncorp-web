0f1be4fc8b649f0e282b98fadc509383450d7028
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2023-12-16 02:39:25 +0700

    style: responsive slider

--- src/components/slider.js
+++ src/components/slider.js
@@ -7,11 +7,11 @@ const Slider = () => {
   return(
     <div className="w-full overflow-hidden">
       <Slide canSwipe={false} pauseOnHover={false}>
-        <div className="relative w-full max-h-[700px]">
-          <StaticImage imgClassName="object-cover w-full max-h-[700px]" src="../images/slider/slider-1.jpg" alt="slide 1"/>
-          <div className="absolute left-0 top-0 w-full h-full inline-flex justify-center items-center bg-black/75">
+        <div className="relative w-full h-screen lg:h-[700px]">
+          <StaticImage className="w-full h-full" imgClassName="object-cover w-full h-full" src="../images/slider/slider-1.jpg" alt="slide 1"/>
+          <div className="absolute left-0 top-0 w-full h-full inline-flex justify-center items-center bg-black/75 px-8 lg:px-0">
             <div className="w-full max-w-7xl text-white">
-              <div className="font-bold text-5xl text-sky-500 mb-5">EVENT</div>
+              <div className="font-bold text-4xl lg:text-5xl text-sky-500 mb-5">EVENT</div>
               <p className="w-full max-w-2xl">
                 From impressive, eye-catching stage design, to flawlessly executed breakout sessions, 
                 our team is here to help you provide exceptional experiences, no matter the size or scope.
@@ -19,11 +19,11 @@ const Slider = () => {
             </div>
           </div>
         </div>
-        <div className="relative w-full max-h-[700px]">
-          <StaticImage imgClassName="object-cover w-full max-h-[700px]" src="../images/slider/slider-2.jpg" alt="slide 1"/>
-          <div className="absolute left-0 top-0 w-full h-full inline-flex justify-center items-center bg-black/75">
+        <div className="relative w-full h-screen lg:h-[700px]">
+          <StaticImage className="w-full h-full" imgClassName="object-cover w-full h-full" src="../images/slider/slider-2.jpg" alt="slide 2"/>
+          <div className="absolute left-0 top-0 w-full h-full inline-flex justify-center items-center bg-black/75 px-8 lg:px-0">
             <div className="w-full max-w-7xl text-white">
-              <div className="font-bold text-5xl text-sky-500 mb-5">PRODUCTION</div>
+              <div className="font-bold text-4xl lg:text-5xl text-sky-500 mb-5">PRODUCTION</div>
               <p className="w-full max-w-2xl">
                 From concept to set-up to on-site support, we are there every step of the way to provide you
                 with the exceptional product and service you deserve.
@@ -31,11 +31,11 @@ const Slider = () => {
             </div>
           </div>
         </div>
-        <div className="relative w-full max-h-[700px]">
-          <StaticImage imgClassName="object-cover w-full max-h-[700px]" src="../images/slider/slider-3.jpg" alt="slide 1"/>
-          <div className="absolute left-0 top-0 w-full h-full inline-flex justify-center items-center bg-black/75">
+        <div className="relative w-full h-screen lg:h-[700px]">
+          <StaticImage className="w-full h-full" imgClassName="object-cover w-full h-full" src="../images/slider/slider-3.jpg" alt="slide 3"/>
+          <div className="absolute left-0 top-0 w-full h-full inline-flex justify-center items-center bg-black/75 px-8 lg:px-0">
             <div className="w-full max-w-7xl text-white">
-              <div className="font-bold text-5xl text-sky-500 mb-5">SUPPLIER</div>
+              <div className="font-bold text-4xl lg:text-5xl text-sky-500 mb-5">SUPPLIER</div>
               <p className="w-full max-w-2xl">
                 We understand how to get audience's attention using the right tools, we're committed to customizing the experience to exceed all expectations.
               </p>
