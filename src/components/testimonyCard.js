4a17520ab88ca074a48e0de12dc3619955fc2a26
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2023-12-18 00:00:50 +0700

    fix: style

--- src/components/testimonyCard.js
+++ src/components/testimonyCard.js
@@ -3,9 +3,9 @@ import { StaticImage } from "gatsby-plugin-image"
 
 
 const TestimonyCard = ({words, name, title}) => (
-  <div className="relative bg-white w-[250px] sm:w-[300px] p-4 sm:p-5 rounded-xl shrink-0">
+  <div className="relative bg-white w-[250px] sm:w-[300px]  h-[370px] p-4 sm:p-5 rounded-xl shrink-0">
     <StaticImage className="absolute bottom-0 right-0 z-0 w-2/3" src="../images/background-testimony.png" alt="testimony background" />
-    <div className="relative flex flex-col justify-between text-sm text-gray-500 shrink-0 h-[270px] z-10">
+    <div className="flex flex-col justify-between text-sm text-gray-500 shrink-0 z-10">
       <p className="mb-5 line-clamp-[10]">{words}</p>
       <div>
         <div className="font-bold">{name}</div>
