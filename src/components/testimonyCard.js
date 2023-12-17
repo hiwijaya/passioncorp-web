542c0ffd35f87d73fdf8f8cc5040dd0943d94c34
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2023-12-17 23:06:08 +0700

    fix: responsive

--- src/components/testimonyCard.js
+++ src/components/testimonyCard.js
@@ -4,12 +4,12 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const TestimonyCard = ({words, name, title}) => (
   <div className="relative bg-white w-[250px] sm:w-[300px] p-4 sm:p-5 rounded-xl shrink-0">
-    {/* <StaticImage className="absolute bottom-0 right-0 w-2/3" src="../images/background-testimony.png" alt="testimony background" /> */}
-    <div className="flex flex-col justify-between relative text-sm text-gray-500 h-full">
+    <StaticImage className="absolute bottom-0 right-0 w-2/3 z-0" src="../images/background-testimony.png" alt="testimony background" />
+    <div className="flex flex-col justify-between relative text-sm text-gray-500 h-full z-10">
       <p className="mb-5 line-clamp-[10]">{words}</p>
       <div>
         <div className="font-bold">{name}</div>
-        <div className="truncate">{title}</div>
+        <div >{title}</div>
       </div>
     </div>
   </div>
