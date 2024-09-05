1d5a00e2eea5c28cd12c175a616fa3fd40eec99f
hiwijaya <happy@perqara.com>
2024-09-05 17:36:27 +0700

    add welcome page

---
+++ src/pages/kafegama/welcome.js
@@ -0,0 +1,79 @@
+import * as React from "react";
+import { useState, useRef } from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import supabase from "../../components/supabaseClient";
+
+
+const WelcomePage = () => {
+
+  const [regId, setRegId] = useState('');
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const inputRef = useRef(null);
+
+  const handleInput = async () => {
+
+    if(loading){
+      return;
+    }
+
+    try{
+      const registrationId = regId.toUpperCase();
+
+      setLoading(true);
+      setName('...');
+      const { data, error } = await supabase.from('participants')
+        .select('*')
+        .eq('reg_id', registrationId.toUpperCase());
+      if(error){
+        throw error;
+      }
+      else{
+        if(data.length > 0){
+          
+          const { error } = await supabase.from('participants')
+            .update({ registered_at: new Date().toISOString() })
+            .eq('id', data[0].id);
+          if(error){
+            throw error;
+          }
+          setName(data[0].name);
+        }
+        else{
+          setName('');
+        }
+      }
+    }
+    catch(error){
+      setName('');
+    }
+    finally{
+      setLoading(false);
+      inputRef.current.value = '';
+    }
+
+
+  }
+
+
+  return(
+    <div className="relative w-full h-screen bg-slate-800 p-6 ">
+      <StaticImage className="absolute left-0 top-0 bottom-0 right-0 z-0" src="../../images/kafegama/background.jpg" alt="background"/>
+      <div className="absolute left-0 top-0 right-0 bottom-0 z-10 flex flex-col justify-center items-center p-6">
+        <div className="text-5xl mb-16">Selamat Datang</div>
+        <div className="font-bold text-7xl">{name}</div>
+        <input ref={inputRef} autoFocus className="opacity-0"
+          onChange={(e) => setRegId(e.target.value)}
+          onKeyDown={(e) => {
+            if(e.key === 'Enter'){
+              handleInput()
+            }
+          }}
+          onBlur={() => inputRef.current.focus()}/>
+        <div className="absolute bottom-20 w-full text-center text-3xl">Terima kasih atas kehadirannya</div>
+      </div>
+    </div>
+  );
+}
+export default WelcomePage;
